31c6bf977a584f4aaa0a044522bcd958305ee1e8
JaeshinJung <123700199+JaeshinJung@users.noreply.github.com>
2025-03-11 21:53:17 +0900

    03/11/2025 2222222
    222222222

--- MenuState.cpp
+++ MenuState.cpp
@@ -15,7 +15,7 @@ void MenuState::Start()
 void MenuState::Update()
 {
 	if (GetAsyncKeyState(VK_F1)) {
-		GameMng::getIns()->stateCtrl.StateChange(STATE::GAME);
+		GameMng::getIns()->stateCtrl.StateChange(new GameState);
 	}
 }
 
